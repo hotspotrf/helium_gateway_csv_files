175b847a44b1cc241ebadb5f7b3597e28745dff0
James Herrick <100632668+dudetechitout@users.noreply.github.com>
2022-04-14 12:31:49 -0400

    Update insert_gateway_inventory.py

--- insert_gateway_inventory.py
+++ insert_gateway_inventory.py
@@ -10,7 +10,7 @@ def csv_export():
         csv_reader = reader(read_obj)
         header = next(csv_reader)
         if header != None:
-            for row in reversed(list(reader(read_obj))):
+            for row in reader(read_obj):
                 db_conn = psycopg2.connect(host="localhost", port="5432", dbname="database_name", user="database_user", password="database_password")
                 db_cursor = db_conn.cursor()
 
@@ -58,6 +58,9 @@ def csv_export():
                     except:
                         gain = 'null'
                         pass
+
+                    if not location:
+                        location = None
 
                     addWits = str(f"INSERT INTO gateway_inventory (address, owner, location, last_poc_challenge, last_poc_onion_key_hash, witnesses, first_block, last_block, nonce, name, first_timestamp, reward_scale, elevation, gain, location_hex, mode, payer) VALUES('{address}', '{owner}', '{location}', {lastcallenge}, '{lasthash}', '{witnesses}', {firstblock}, {lastblock}, {nonce}, '{name}', '{firsttimestamp}', {reward}, {elevation}, {gain}, '{locationhex}', '{mode}', '{payer}') ON CONFLICT (address) DO NOTHING")
                     
