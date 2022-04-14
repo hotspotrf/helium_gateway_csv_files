4b26d026898068e0377f3445a7a44be8c9294a0e
James Herrick <100632668+dudetechitout@users.noreply.github.com>
2022-04-14 03:05:56 -0400

    Update insert_gateway_inventory.py

--- insert_gateway_inventory.py
+++ insert_gateway_inventory.py
@@ -3,27 +3,61 @@ from io import StringIO
 from csv import reader
 
 def csv_export():
-    nones = lambda n: [None for _ in range(n)]
+    nones = lambda n: ['null' for _ in range(n)]
     address, owner, location, lastcallenge, lasthash, witnesses, firstblock, lastblock, nonce, name, firsttimestamp, reward, elevation, gain, locationhex, mode, payer = nones(17)
     
     with open('inventory.csv', 'r') as read_obj:
         csv_reader = reader(read_obj)
         header = next(csv_reader)
         if header != None:
-            for row in csv_reader:
+            for row in reversed(list(reader(read_obj))):
                 db_conn = psycopg2.connect(host="localhost", port="5432", dbname="database_name", user="database_user", password="database_password")
                 db_cursor = db_conn.cursor()
 
                 try:
                     address, owner, location, lastcallenge, lasthash, witnesses, firstblock, lastblock, nonce, name, firsttimestamp, reward, elevation, gain, locationhex, mode, payer = row[0].split("|")
                     
-                    lastcallenge=int(lastcallenge)
-                    firstblock=int(firstblock)
-                    lastblock=int(lastblock)
-                    nonce=int(nonce)
-                    reward=float(reward)
-                    elevation=int(elevation)
-                    gain=int(gain)
+                    try:
+                        lastcallenge=int(lastcallenge)
+                    except:
+                        lastcallenge = 'null'
+                        pass
+
+                    try:
+                        firstblock=int(firstblock)
+                    except:
+                        firstblock = 'null'
+                        pass
+
+                    try:
+                        lastblock=int(lastblock)
+                    except:
+                        lastblock = 'null'
+                        pass
+
+                    try:
+                        nonce=int(nonce)
+                    except:
+                        nonce = 'null'
+                        pass
+
+                    try:
+                        reward=float(reward)
+                    except:
+                        reward = 'null'
+                        pass
+
+                    try:
+                        elevation=int(elevation)
+                    except:
+                        elevation = 'null'
+                        pass
+
+                    try:
+                        gain=int(gain)
+                    except:
+                        gain = 'null'
+                        pass
 
                     addWits = str(f"INSERT INTO gateway_inventory (address, owner, location, last_poc_challenge, last_poc_onion_key_hash, witnesses, first_block, last_block, nonce, name, first_timestamp, reward_scale, elevation, gain, location_hex, mode, payer) VALUES('{address}', '{owner}', '{location}', {lastcallenge}, '{lasthash}', '{witnesses}', {firstblock}, {lastblock}, {nonce}, '{name}', '{firsttimestamp}', {reward}, {elevation}, {gain}, '{locationhex}', '{mode}', '{payer}') ON CONFLICT (address) DO NOTHING")
                     
